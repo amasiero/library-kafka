d7a346de53684f2d4e5e9d4ed109ec9baae051df
Andrey Masiero <andreymasiero@gmail.com>
2023-03-28 08:43:58 +0100

    integrate with producer

--- library-events-producer/src/main/java/me/amasiero/library/controller/LibraryEventsController.java
+++ library-events-producer/src/main/java/me/amasiero/library/controller/LibraryEventsController.java
@@ -1,6 +1,9 @@
 package me.amasiero.library.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import lombok.AllArgsConstructor;
 import me.amasiero.library.domain.LibraryEvent;
+import me.amasiero.library.producer.LibraryEventProducer;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,9 +11,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 public class LibraryEventsController {
+
+    private final LibraryEventProducer libraryEventProducer;
+
     @PostMapping("/v1/library-event")
-    public ResponseEntity<LibraryEvent> create(@RequestBody LibraryEvent libraryEvent) {
+    public ResponseEntity<LibraryEvent> create(@RequestBody LibraryEvent libraryEvent) throws JsonProcessingException {
+        libraryEventProducer.sendLibraryEvent(libraryEvent);
         return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
     }
 }
