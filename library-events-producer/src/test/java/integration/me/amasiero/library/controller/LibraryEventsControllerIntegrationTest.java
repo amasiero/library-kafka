13b44628baf00d773abf04ec0800e63ab951f16e
Andrey Masiero <andreymasiero@gmail.com>
2023-04-08 21:20:29 +0100

    feat: add test to producer controller

--- library-events-producer/src/test/java/integration/me/amasiero/library/controller/LibraryEventsControllerIntegrationTest.java
+++ library-events-producer/src/test/java/integration/me/amasiero/library/controller/LibraryEventsControllerIntegrationTest.java
@@ -1,2 +1,60 @@
-package me.amasiero.library.controller;public class LibraryEventsControllerIntegrationTest {
+package me.amasiero.library.controller;
+
+import me.amasiero.library.domain.Book;
+import me.amasiero.library.domain.LibraryEvent;
+import me.amasiero.library.domain.LibraryEventType;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.*;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.test.context.TestPropertySource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@EmbeddedKafka(topics = {"library-events"}, partitions = 3)
+@TestPropertySource(properties = {
+        "spring.kafka.producer.bootstrap-servers=${spring.embedded.kafka.brokers}",
+        "spring.kafka.admin.properties.bootstrap.servers=${spring.embedded.kafka.brokers}"
+})
+public class LibraryEventsControllerIntegrationTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    @DisplayName("When POST is requested returns http status created (201)")
+    public void whenPostIsRequested_returnsHttpStatusCreated() {
+        // Given
+        Book book = Book.builder()
+                .id(123L)
+                .author("test")
+                .name("test")
+                .build();
+
+        LibraryEvent event = LibraryEvent.builder()
+                .id(null)
+                .book(book)
+                .type(LibraryEventType.CREATE)
+                .build();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("content-type", MediaType.APPLICATION_JSON_VALUE);
+
+        HttpEntity<LibraryEvent> request = new HttpEntity<>(event, headers);
+
+        // When
+        ResponseEntity<LibraryEvent> responseEntity = restTemplate.exchange(
+                "/v1/library-event",
+                HttpMethod.POST,
+                request,
+                LibraryEvent.class
+        );
+
+        // Then
+        assertEquals(HttpStatus.CREATED, responseEntity.getStatusCode());
+    }
 }
