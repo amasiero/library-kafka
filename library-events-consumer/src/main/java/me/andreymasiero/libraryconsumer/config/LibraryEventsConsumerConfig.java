21f6a9c94f3d3c5456a71ebefe09f9fdd0531d6a
Andrey Masiero <andreymasiero@gmail.com>
2023-04-29 15:47:35 +0100

    feat: adding consumer w/ manual offset

--- library-events-consumer/src/main/java/me/andreymasiero/libraryconsumer/config/LibraryEventsConsumerConfig.java
+++ library-events-consumer/src/main/java/me/andreymasiero/libraryconsumer/config/LibraryEventsConsumerConfig.java
@@ -1,9 +1,34 @@
 package me.andreymasiero.libraryconsumer.config;
 
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.kafka.ConcurrentKafkaListenerContainerFactoryConfigurer;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.listener.ContainerProperties;
 
 @Configurable
 @EnableKafka
+@AllArgsConstructor
 public class LibraryEventsConsumerConfig {
+
+    private final KafkaProperties properties;
+    @Bean
+    @ConditionalOnMissingBean(name = "kafkaListenerContainerFactory")
+    ConcurrentKafkaListenerContainerFactory<?, ?> kafkaListenerContainerFactory(
+            ConcurrentKafkaListenerContainerFactoryConfigurer configurer,
+            ObjectProvider<ConsumerFactory<Object, Object>> kafkaConsumerFactory) {
+        ConcurrentKafkaListenerContainerFactory<Object, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        configurer.configure(factory, kafkaConsumerFactory
+                .getIfAvailable(() -> new DefaultKafkaConsumerFactory<>(this.properties.buildConsumerProperties())));
+        factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL);
+        return factory;
+    }
+
 }
